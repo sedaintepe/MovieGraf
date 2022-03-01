4c4084114f551ee6a388c1aef982fabd3c744168
Seda <sedaintepe995@gmail.com>
2022-03-01 22:29:25 +0300

    Add project files.

---
+++ Plugins.DataStore.InMemory/CategoryInMemoryRepository.cs
@@ -0,0 +1,27 @@
+﻿using CoreBusiness;
+using System;
+using System.Collections.Generic;
+using UseCases.DataClassPluginInterfaces;
+
+namespace Plugins.DataStore.InMemory
+{
+    public class CategoryInMemoryRepository : ICategoryRepository
+    {
+        private List<Category> _categories;
+        public CategoryInMemoryRepository()
+        {
+            _categories = new List<Category>()
+            {
+                new Category{Id=1,Name="Action",Description="Action" },
+                new Category{Id=2,Name="Adventure",Description="Adventure" },
+                new Category{Id=3,Name="Comedy",Description="Comedy" },
+                new Category{Id=4,Name="Drama",Description="Drama" },
+                new Category{Id=5,Name="Crime",Description="Crime" }
+            };
+        }
+        public IEnumerable<Category> GetRepositories()
+        {
+            return _categories;  
+        }
+    }
+}
---
+++ UseCases/ViewCategoriesUseCases.cs
@@ -0,0 +1,24 @@
+﻿using CoreBusiness;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UseCases.DataClassPluginInterfaces;
+
+namespace UseCases
+{
+    public class ViewCategoriesUseCases
+    {
+        private readonly ICategoryRepository CategoryRepository;
+        public ViewCategoriesUseCases(ICategoryRepository categoryRepository)
+        {
+            CategoryRepository = categoryRepository;
+        }
+
+        public IEnumerable<Category> Execute()
+        {
+           return CategoryRepository.GetRepositories();
+        }
+    }
+}
